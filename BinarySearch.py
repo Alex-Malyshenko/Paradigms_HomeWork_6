75254c2ebb67c3c2812afb5fb78bd63da1d6b767
Alex-Malyshenko <alis_mal@mail.ru>
2023-12-09 23:29:50 +0300

    end

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,11 +1,21 @@
 
-def powerProcedure(arrayIn, a):
+def power_search(arr, target):
+    low = 0
+    high = len(arr) - 1
 
+    while low <= high:
+        mid = (low + high) // 2
 
-
-
-
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
 
 
 if __name__ == "__main__":
-    powerProcedure([], int(input("Введите число: ")))
+    g = [2, 5, 4, 8, 9, 11, -5, -4, 0, 7 ]
+    print(sorted(g))
+    print(power_search(sorted(g), int(input("Введите число: "))))
+
